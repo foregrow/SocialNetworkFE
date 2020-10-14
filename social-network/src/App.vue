79b4696d9c3340988ffe1bb2e778fe94d3e9cc69
Filip <filip.vincic22@gmail.com>
2020-10-14 16:42:18 +0200

    initial

---
+++ social-network/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <div class="mt-5 col-3">
+        <form>
+          <h2 class="h2">Login</h2>
+
+          <div class="form-group">
+            <input type="text" placeholder="Username" class="form-control" />
+          </div>
+
+          <div class="form-group">
+            <input type="password" placeholder="Lozinka" class="form-control" />
+          </div>
+          <button class="btn btn-primary" type="submit">Log in</button>
+        </form>
+      </div>
+      <div class="mt-5 col-3">
+        <h2>Register</h2>
+        <button class="btn btn-primary" type="submit">Submit registration</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
